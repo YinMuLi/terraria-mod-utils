24920e72e8dd71b3b614e30a6483e1f01a5718fb
YinMu <1621780679@qq.com>
2023-12-10 20:09:52 +0800

    2023/12/10 周日 20:09 AUTO

--- Branch.cs
+++ Branch.cs
@@ -1,8 +1,21 @@
+using Branch.Content.Buffs;
+using Branch.Content.Items;
 using Terraria.ModLoader;
 
 namespace Branch
 {
     public class Branch : Mod
     {
+        public override void PostSetupContent()
+        {
+            if (ModLoader.TryGetMod("ImproveGame", out Mod improveGame))
+            {
+                improveGame.Call(
+                    "AddStation",//加入你自己的增益站1
+                    ModContent.ItemType<StationCake>(),
+                    ModContent.BuffType<OriginalStationBuff>()
+                );
+            }
+        }
     }
 }

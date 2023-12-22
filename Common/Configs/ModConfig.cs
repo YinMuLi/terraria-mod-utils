ba79f1d51b8251b6780c6c437c0346b0d2d52118
YinMu <1621780679@qq.com>
2023-12-22 12:09:19 +0800

    2023/12/22 周五 12:09 AUTO

--- Common/Configs/ModConfig.cs
+++ Common/Configs/ModConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Threading.Tasks.Dataflow;
 using Terraria.ModLoader.Config;
 
 namespace Branch.Common.Configs
@@ -9,15 +10,6 @@ namespace Branch.Common.Configs
         public override ConfigScope Mode => ConfigScope.ClientSide;
 
         public override void OnLoaded() => Instance = this;
-
-        /// <summary>
-        /// 鱼线数量
-        /// </summary>
-        [Range(0, 50)]
-        [Increment(1)]
-        [DefaultValue(5)]
-        [Slider]
-        public int LuresAmount;
 
         /// <summary>
         /// 是否生成Tom NPC
@@ -37,10 +29,40 @@ namespace Branch.Common.Configs
         [DefaultValue(true)]
         public bool ExtraWingSlot;
 
+        [Header("Fishing")]
         /// <summary>
         /// 自动钓鱼
         /// </summary>
         [DefaultValue(true)]
         public bool AutoFish;
+
+        /// <summary>
+        /// 鱼线数量
+        /// </summary>
+        [Range(0, 50)]
+        [Increment(1)]
+        [DefaultValue(5)]
+        [Slider]
+        public int LuresAmount;
+
+        /// <summary>
+        /// 拉杆延迟时间
+        /// </summary>
+        [Range(0f, 1f)]
+        [Increment(0.1f)]
+        [DefaultValue(0.5f)]
+        public float PullWaitTimer;
+
+        /// <summary>
+        /// 捕获宝箱
+        /// </summary>
+        [DefaultValue(true)]
+        public bool CatchCrate;
+
+        /// <summary>
+        /// 捕获饰品
+        /// </summary>
+        [DefaultValue(false)]
+        public bool CatchAccessories;
     }
 }
--- Common/Players/AutoFishPlayer.cs
+++ Common/Players/AutoFishPlayer.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Branch.Common.Configs;
+using Branch.Common.Utils;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
@@ -15,18 +17,11 @@ namespace Branch.Common.Players
 
         internal Point mousePos;
         internal bool autoMode;
-        private int waitTime = 60;
 
         /// <summary>
-        /// 拉杆
-        /// fishingPlole:钓鱼竿
+        /// 收杆延迟时间
         /// </summary>
-        private bool pullFishingPole;
-
-        public override bool IsLoadingEnabled(Mod mod)
-        {
-            return base.IsLoadingEnabled(mod);
-        }
+        internal int pullWaitTimer;
 
         public override void Load()
         {
@@ -40,37 +35,50 @@ namespace Branch.Common.Players
             On_Player.ItemCheck_Shoot -= OnShoot;
         }
 
+        public override void ResetEffects()
+        {
+            modInvoke = false;
+            autoMode = false;
+            pullWaitTimer = 0;
+            mousePos = default;
+        }
+
+        //一秒60次
         public override void PreUpdate()
         {
             if (Player.whoAmI != Main.myPlayer) return;
-            if (pullFishingPole)
+            /**
+             * 自动模式：自动抛竿，自动收杆
+             */
+            modInvoke = false;
+            if (autoMode)
             {
-                waitTime--;
-                if (waitTime == 0)
+                if (pullWaitTimer > 0 && --pullWaitTimer == 0)
                 {
                     //自动收杆
                     Player.controlUseItem = true;
                     Player.releaseUseItem = true;
                     modInvoke = true;
-                    Player.ItemCheck();
-                    pullFishingPole = false;
-                    waitTime = 60;
-                    //10帧后再抛竿
-                    if (Player.HeldItem.fishingPole <= 0)
-                    {
-                        //玩家不是手持鱼竿，关闭自动钓鱼
-                        autoMode = false;
-                        return;
-                    }
-                    if (IsBobberActive(Player.whoAmI))
-                    {
-                        //还有浮漂处于激活，不能抛竿
-                        return;
-                    }
-                    Player.controlUseItem = true;
-                    Player.releaseUseItem = true;
+                    //玩家检查物品，会触发ItemCheck的事件
                     Player.ItemCheck();
                 }
+                if (Player.HeldItem.fishingPole == 0)
+                {
+                    //玩家不是手持鱼竿，关闭自动钓鱼
+                    autoMode = false;
+                    return;
+                }
+                if (IsBobberActive(Player.whoAmI))
+                {
+                    //还有浮漂处于激活，即处于钓鱼状态不能抛竿
+                    return;
+                }
+                Main.mouseX = mousePos.X - (int)Main.screenPosition.X;
+                Main.mouseY = mousePos.Y - (int)Main.screenPosition.Y;
+                Player.controlUseItem = true;
+                Player.releaseUseItem = true;
+                modInvoke = true;
+                Player.ItemCheck();
             }
         }
 
@@ -93,12 +101,13 @@ namespace Branch.Common.Players
 
         private void OnShoot(On_Player.orig_ItemCheck_Shoot orig, Player self, int i, Item sItem, int weaponDamage)
         {
-            if (self.whoAmI == Main.myPlayer && self.TryGetModPlayer(out AutoFishPlayer player) && !player.modInvoke && sItem.fishingPole > 0)
+            if (ModConfig.Instance.AutoFish && self.whoAmI == Main.myPlayer && self.TryGetModPlayer(out AutoFishPlayer player) && !player.modInvoke && sItem.fishingPole > 0)
             {
                 //既然它给你了player self就保险一点，获取钓鱼玩家
                 //玩家手动抛竿，记录抛竿的位置,启动自动钓鱼
                 player.mousePos = Main.MouseWorld.ToPoint();
                 player.autoMode = true;
+                player.pullWaitTimer = 0;//重置拉杆等待时间
             }
             orig.Invoke(self, i, sItem, weaponDamage);
         }
@@ -106,7 +115,7 @@ namespace Branch.Common.Players
         private bool OnCheckFishingBobbers(On_Player.orig_ItemCheck_CheckFishingBobbers orig, Player self, bool canUse)
         {
             bool flag = orig.Invoke(self, canUse);//false:收杆
-            if (!flag && self.whoAmI == Main.myPlayer && self.TryGetModPlayer(out AutoFishPlayer player) && !player.modInvoke)
+            if (!flag && ModConfig.Instance.AutoFish && self.whoAmI == Main.myPlayer && self.TryGetModPlayer(out AutoFishPlayer player) && !player.modInvoke)
             {
                 //玩家手动收杆，取消自动钓鱼
                 player.autoMode = false;
@@ -117,11 +126,29 @@ namespace Branch.Common.Players
         //成功咬钩
         public override void CatchFish(FishingAttempt attempt, ref int itemDrop, ref int npcSpawn, ref AdvancedPopupRequest sonar, ref Vector2 sonarPosition)
         {
-            if (ItemID.Sets.IsFishingCrate[itemDrop])
-            {
-                pullFishingPole = true;
-            }
             base.CatchFish(attempt, ref itemDrop, ref npcSpawn, ref sonar, ref sonarPosition);
+            if (ModConfig.Instance.AutoFish && pullWaitTimer == 0 && itemDrop >= 0)
+            {
+                if (!Player.sonarPotion)
+                {
+                    //玩家没有声呐药水，直接拉杆
+                    pullWaitTimer = (int)(ModConfig.Instance.PullWaitTimer * 60);
+                    return;
+                }
+                if (ItemID.Sets.IsFishingCrate[itemDrop] && ModConfig.Instance.CatchCrate)
+                {
+                    //宝箱
+                    pullWaitTimer = (int)(ModConfig.Instance.PullWaitTimer * 60);
+                    return;
+                }
+                var item = new Item(itemDrop);
+                if (item.accessory && ModConfig.Instance.CatchAccessories)
+                {
+                    //饰品
+                    pullWaitTimer = (int)(ModConfig.Instance.PullWaitTimer * 60);
+                    return;
+                }
+            }
         }
     }
 }

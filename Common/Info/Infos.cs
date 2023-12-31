25135498345521db1ca8a8d294764046bab2c921
YinMu <1621780679@qq.com>
2023-12-31 11:05:27 +0800

    2023/12/31 周日 11:05 AUTO

---
+++ Common/Info/Infos.cs
@@ -0,0 +1,36 @@
+﻿using Humanizer;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace Branch.Common.Info
+{
+    /// <summary>
+    /// Minion:仆从 Sentry:哨兵 Turrets:炮塔
+    /// </summary>
+    internal class MinionInfo : InfoDisplay
+    {
+        public override bool Active() => true;
+
+        public override string DisplayValue(ref Color displayColor, ref Color displayShadowColor)
+        {
+            int maxMinion = Main.LocalPlayer.maxMinions;
+            //有的召唤物单位不是1,保留两位小数
+            float minionCount = (float)Math.Round(Main.LocalPlayer.slotsMinions, 2);
+
+            int sentryCount = 0;//哨兵
+            for (int i = 0; i < Main.maxProjectiles; i++)
+            {
+                var proj = Main.projectile[i];
+                if (proj.active && proj.sentry && proj.owner == Main.myPlayer)
+                {
+                    sentryCount++;
+                }
+            }
+            return Language.GetTextValue("Mods.UI.InfoDisplay.Minion")
+                .FormatWith(minionCount, maxMinion, sentryCount, Main.LocalPlayer.maxTurrets);
+        }
+    }
+}

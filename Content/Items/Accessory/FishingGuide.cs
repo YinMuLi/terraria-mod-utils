ee8c0c7caed5f95ce71d775f9f54d494263a29a6
YinMu <1621780679@qq.com>
2024-02-03 17:12:13 +0800

    2024/02/03 周六 17:12 AUTO

--- Content/Items/Accessory/FishingGuide.cs
+++ Content/Items/Accessory/FishingGuide.cs
@@ -4,7 +4,7 @@ using Terraria.ModLoader;
 
 namespace Branch.Content.Items.Accessory
 {
-    internal class FishBone : ModItem
+    internal class FishingGuide : ModItem
     {
         public override void SetDefaults()
         {

1bccdf9f453435f8d448f4f59af5741d67063d57
YinMu <1621780679@qq.com>
2024-02-26 14:19:38 +0800

    2024/02/26 周一 14:19 AUTO

---
+++ Common/Commands/InfoCommand.cs
@@ -0,0 +1,22 @@
+﻿using Terraria.ModLoader;
+
+namespace Branch.Common.Commands
+{
+    internal class InfoCommand : ModCommand
+    {
+        public override string Command => "info";
+
+        public override CommandType Type => CommandType.Chat;
+
+        public override string Description => "";
+
+        public override void Action(CommandCaller caller, string input, string[] args)
+        {
+            //if (args.Length < 1) return;
+            //if (int.TryParse(args[0], out int type))
+            //{
+            //    ModUtils.GiveItem(caller.Player, type);
+            //}
+        }
+    }
+}
---
+++ Content/Items/BossSummon/SummonBloodNautilus.cs
@@ -0,0 +1,49 @@
+﻿using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Branch.Content.Items.BossSummon
+{
+    internal class SummonBloodNautilus : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            NPCID.Sets.MPAllowedEnemies[NPCID.BloodNautilus] = true;//多人模式能被召唤
+            ItemID.Sets.SortingPriorityBossSpawns[Type] = 11; // 松露虫
+        }
+
+        public override void SetDefaults()
+        {
+            Item.useAnimation = 10;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.rare = ItemRarityID.Lime;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            //困难模式血月
+            return Main.hardMode && Main.bloodMoon && !NPC.AnyNPCs(NPCID.BloodNautilus);
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            SoundEngine.PlaySound(SoundID.Roar, player.Center);
+            if (Main.netMode != NetmodeID.MultiplayerClient)
+                NPC.SpawnOnPlayer(player.whoAmI, NPCID.BloodNautilus);
+            else
+                NetMessage.SendData(MessageID.SpawnBossUseLicenseStartEvent, number: player.whoAmI, number2: NPCID.BloodNautilus);
+
+            return true;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient(ItemID.BloodMoonStarter).
+                AddTile(TileID.DemonAltar).
+                Register();
+        }
+    }
+}

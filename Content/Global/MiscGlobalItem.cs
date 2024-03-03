2de9e65e12210e16df94b344cd06863be71de7e8
YinMu <1621780679@qq.com>
2024-03-03 18:08:18 +0800

    2024/03/03 周日 18:08 AUTO

--- Content/Global/MiscGlobalItem.cs
+++ Content/Global/MiscGlobalItem.cs
@@ -1,5 +1,6 @@
 ﻿using Branch.Common.Extensions;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -30,6 +31,29 @@ namespace Branch.Content.Global
                     , ItemID.LifeFruit, minQuantity: 10, maxQuantity: 15);
                     break;
             }
+        }
+
+        private void Reforge(Item item, int prefixID)
+        {
+            if (item.prefix != prefixID && item.CanApplyPrefix(prefixID))
+            {
+                item.ResetPrefix();
+                item.Prefix(prefixID);
+                ItemLoader.PostReforge(item);
+            }
+        }
+
+        public override void OnCreated(Item item, ItemCreationContext context)
+        {
+            //召唤
+            if (item.DamageType == DamageClass.SummonMeleeSpeed) Reforge(item, PrefixID.Legendary);//传奇
+            if (item.DamageType == DamageClass.Summon) Reforge(item, PrefixID.Ruthless);//无情
+            //战士
+            if (item.DamageType == DamageClass.Melee || item.DamageType == DamageClass.MeleeNoSpeed) Reforge(item, PrefixID.Legendary);//传奇
+            //射手
+            if (item.DamageType == DamageClass.Ranged) Reforge(item, PrefixID.Unreal);//虚幻
+            //法师
+            if (item.DamageType == DamageClass.Magic) Reforge(item, PrefixID.Mythical);//神话
         }
     }
 }
